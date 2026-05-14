26f9275ab7d814032b85af88aeed808d0d13f2c0
shrutilad20 <shrutilad35@gmail.com>
2026-05-14 23:01:20 +0530

    initial

---
+++ backend/src/test/java/com/studentstash/studentstash/StudentstashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentstash.studentstash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentstashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
